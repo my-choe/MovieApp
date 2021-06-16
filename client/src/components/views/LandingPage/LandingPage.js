2336f3fc9a3b49bb35e78a9e07c997084ea50d5e
my-choe <my_choe@naver.com>
2021-06-16 17:22:28 +0900

    Landing Page 만들기

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,10 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { FaCode } from "react-icons/fa";
-
+import { API_URL, API_KEY, IMAGE_BASE_URL } from '../../Config'
+import MainImage from './Sections/MainImage'
 function LandingPage() {
+
+    const [Movies, setMovies] = useState([])
+    const [MainMovieImage, setMainMovieImage] = useState()
+
+
+    useEffect(() => {
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=ko-Korean&page=1`;
+
+        fetch(endpoint)
+        .then(response => response.json())
+        .then(response => {
+            console.log(response)
+            setMovies([response.result])
+            setMainMovieImage(response.results[0])
+        });
+
+    }, [])
+
+
     return (
-        <>
-        </>
+        <div style={{ width: '100%', margin: '0'}}>
+
+            { /* Main Image */}
+            {MainMovieImage && 
+                <MainImage 
+                    image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`}
+                    title={`${MainMovieImage.title}`}
+                    overview={`${MainMovieImage.overview}`}
+                    />
+            }  
+            <div style={{ width: '85%', margin: '1rem auto' }}>
+
+                <h2>최신 영화</h2>
+                <hr />
+
+                { /* Movie Grid Cards */}
+
+            </div>
+
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <button>더보기</button>
+            </div>
+        </div>
     )
 }
 
